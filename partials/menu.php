34c383bfa07ba9bad098ecf4b2ec59fca4a4cf61
Faris Odobasic <Faris Odobasic>
2016-12-26 01:02:05 +0100

    first

---
+++ partials/menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar-default" id ="menu" style="border-radius:0px">
+    <div class="container" >
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!--<a class="navbar-brand" href="http://jobfair.ba/"><img class="logo-img" src="<?=$url_home;?>img/logo/jf-logo-white.png" style="margin-left:-20px;"></a>-->
+                </div>
+
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
+                    <ul class="nav navbar-nav navbar-right" id="navbarMain">
+                        <!--<li><a href="<?=$url_home;?>ucesnici">Novosti</a></li>-->
+                        <li><a href="http://www.jobfair.ba/novosti">Home</a></li>
+                        <li><a href="http://www.jobfair.ba/mediji">About</a></li>
+                        <li><a href="http://www.jobfair.ba/ucesnici"> Services</a></li>
+                        <!-- <li><a href="#ucesnici" >Učesnici</a></li>
+                        <li><a href="#postani-partner" >Postani partner</a></li>-->
+                        <li><a href="http://www.jobfair.ba/galerija">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<a href="#"class="top"><img src="img/uparrow.png" align="middle"/></a>
